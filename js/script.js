eabfa53d28350e20feea116c1c7efa783e444112
sherby <fafonso@greenbyweb.eu>
2017-11-14 10:58:00 +0000

    links fixed. reservas kinda done. page sizing still needs fixing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 function pulse(unique) {
 	$("#" + unique).transition('pulse');
 }
+
+$(function(){
+	$('#example2').calendar({
+		type: 'date'
+		});
+
+	$('.menu .item').tab();
+
+	$('#example3').calendar({
+  		type: 'time'
+	});
+});
