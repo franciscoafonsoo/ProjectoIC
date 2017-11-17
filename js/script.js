6e4f701412af92dab25f0611fa209e16dc52fe07
sherby <fafonso@greenbyweb.eu>
2017-11-17 16:20:09 +0000

    reservas v2

--- js/script.js
+++ js/script.js
@@ -3,13 +3,75 @@ function pulse(unique) {
 }
 
 $(function(){
+
+	// Calendario
+
 	$('#example2').calendar({
 		type: 'date'
-		});
+	});
 
-	$('.menu .item').tab();
+	// Hora
 
 	$('#example3').calendar({
-  		type: 'time'
+		type: 'time'
 	});
+
+	// Form Reservas
+
+	$('#efectuar').form({
+		fields: {
+			data: {
+				identifier: 'data',
+				rules: [
+				{
+					type   : 'empty',
+					prompt : 'Por favor, escolha uma data.'
+				}
+				]
+			},
+			hora: {
+				identifier: 'hora',
+				rules: [
+				{
+					type   : 'empty',
+					prompt : 'Por favor, escolha uma hora.'
+				}
+				]
+			},
+			pessoas: {
+				identifier: 'pessoas',
+				rules: [
+				{
+					type   : 'empty',
+					prompt : 'Por favor, escolha um número de pessoas.'
+				}
+				]
+			}
+		}
+	});
+
+	$('.button').click( function function_name(argument) {
+		$('#cenas').modal('show');
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
 });
