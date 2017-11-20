a6ef136255428c09e3623c47fd584951510ae440
sherby <fafonso@greenbyweb.eu>
2017-11-20 13:22:36 +0000

    cenas

--- js/reservas.js
+++ js/reservas.js
@@ -1,16 +1,29 @@
 $(function(){
 
+	/*
+	1. Começar pedido. Começa em qualquer página (modificar para "nova refeição" soon)
+
+		- Verificar se existe a chave 'refeicao' na sessionStorage.
+			- Se sim: Obtem e faz update aos valores na página
+			- Se não: Cria os valores e guarda em sessionStorage
+ 	*/
+
+	if(!localStorage.getItem('reservas')) {
+		localStorage.setItem('reservas', JSON.stringify({items: []}))
+		localStorage.setItem('id', JSON.stringify({total: 0}))
+	}
+
 	// Calendario
 
 	$('#example2').calendar({
 		type: 'date'
-	});
+	})
 
 	// Hora
 
 	$('#example3').calendar({
 		type: 'time'
-	});
+	})
 
 	// Form Reservas
 
@@ -44,35 +57,44 @@ $(function(){
 				]
 			}
 		}
-	});
+	})
 
 	// efectuar reserva e confirmar
 	$('.button').click( function () {
 		if( $('#efectuar').form('is valid')) {
 
-			var uniq = 'id' + (new Date()).getTime();
-			var data 		= document.getElementById("data").value;
-			var pessoas 	= document.getElementById("pessoas").value;
-			var hora 		= document.getElementById("hora").value;
-			var telemovel 	= document.getElementById("telemovel").value;
+			var already = JSON.parse(localStorage.getItem('reservas'))
+			var increm	= JSON.parse(localStorage.getItem('id'))
 
-			$('#contentData').text('Data da Reserva: ' + data);
-			$('#contentHora').text('Hora da Reserva: ' + hora);
-			$('#contentPessoas').text('Nº de Pessoas: ' + pessoas);
-			$('#contentTelemovel').text('Nº de Telemóvel: ' + telemovel);
+			increm.total++
+			var data 		= document.getElementById("data").value
+			var pessoas 	= document.getElementById("pessoas").value
+			var hora 		= document.getElementById("hora").value
+			var telemovel 	= document.getElementById("telemovel").value
 
-			var dict = {data: data, pessoas: pessoas, hora: hora, telemovel: telemovel}
+			$('#contentData').text('Data da Reserva: ' + data)
+			$('#contentHora').text('Hora da Reserva: ' + hora)
+			$('#contentPessoas').text('Nº de Pessoas: ' + pessoas)
+			$('#contentTelemovel').text('Nº de Telemóvel: ' + telemovel)
+
+			already.items.push({
+				id: increm.total, 
+				data: data, 
+				pessoas: pessoas, 
+				hora: hora, telemovel: telemovel
+			})
 
   			$('#cenas').modal({
   			  onApprove : function() {
 
   			  	// se confirmar os dados, guarda na localStorage
-  			    localStorage.setItem(uniq, JSON.stringify(dict));
-  			    alert('Reserva Efectuada');
-				window.location.replace("../index.html");
+  			    localStorage.setItem('reservas', JSON.stringify(already))
+  			    localStorage.setItem('id', JSON.stringify(increm))
+  			    alert('Reserva Efectuada')
+				window.location.replace("../index.html")
   			    
   			  }
-  			}).modal('show');
+  			}).modal('show')
 		}
-	});
-});
+	})
+})
