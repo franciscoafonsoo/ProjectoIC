00e3bea6bfad400761434c18d919cebeced17633
sherby <fafonso@greenbyweb.eu>
2017-12-06 11:52:34 +0000

    nova tabela, items agrupados

--- js/script.js
+++ js/script.js
@@ -19,7 +19,8 @@ $(function(){
 
 		jQuery.each(already.items, function (index, item) {
 			// insert missing lines in the table
-			var cenas = '<tr class="' + item.identify + '"><td>' + item.name + '</td>><td>' + item.price + '</td><td><i id="' + item.identify + '" class="remove red icon" onClick="remove(this.id)"></i></td></tr>'
+			var newprice = item.price * item.qty
+			var cenas = '<tr class="' + item.identify + '"><td>' + item.name + '</td>><td>' + item.qty + '</td><td><i id="' + item.identify + '" class="remove red icon" onClick="remove(this.id)"></i></td><td>' + newprice + '€</td></tr>'
 			$('.ui.huge.table').append(cenas)
 			$('#total').text(conta.total + '€')
 			$('#pedido').html('<i class="shop icon"></i>Meu Pedido: ' + conta.total + '€')
@@ -48,10 +49,10 @@ $(function(){
 		window.location.href = "../pagamento/bitcoin.html";
 	})
 
-	/* accordion on help page */
+	/* accordion on 'ajuda' */
 	$('.ui.accordion').accordion()
 
-	/* tab on new meal */
+	/* tab on 'refeiçoes' */
 	$('.menu .item').tab()
 })
 
@@ -77,7 +78,7 @@ function pulse(unique) {
 	var already = JSON.parse(sessionStorage.getItem('refeicao'))
 	var conta 	= JSON.parse(sessionStorage.getItem('total'))
 	var name 	= $("#" + unique).find(".alignleft").html()
-	var price	= $("#" + unique).find(".alignright").html()
+	var price	= $("#" + unique).find(".alignright").html().slice(0,-1)
 	var extra 	= $("#" + unique).find("#extra").html()
 	var imagem 	= $("#" + unique).find(".ui.large.fluid.image").attr("src")
 	// geracao de um id unico e verificar se já existe um id igual
@@ -87,34 +88,61 @@ function pulse(unique) {
 
 	// por info necessaria no modal
 	$('#contentProduto').html(name);
-	$('#contentProduto').append('<p id="contentPreco" class="alignright">' + price + '</p>')
+	$('#contentProduto').append('<p id="contentPreco" class="alignright">' + price + '€</p>')
 	$('#contentConstituintes').html(extra);
 	$('#contentImagem').attr('src', "" + imagem);
 
 	$('#algo').modal({
 		onApprove : function() {
-			// se confirmar, parse do valor
-			conta.total += parseFloat(price.slice(0,-1))
-			// valor só com duas casas decimais
-			conta.total.toFixed(3)
-
-			// adiciona os valores ao json
-			already.items.push({
-				name: name,
-				price: price,
-				qty: 1,
-				identify: usar
+			// control variable and qty
+			var control = 0
+			var quantity = 1
+			// get table line to be modified
+			var modify
+			// verificar se já existe
+			jQuery.each(already.items, function (index, item) {
+				if (item.name == name) {
+					// found same item
+					control = 1
+					// get quantify for table
+					quantity = item.qty
+					// update qty
+					item.qty = item.qty + 1
+					// parse do valor
+					conta.total += parseFloat(price)
+					// valor só com duas casas decimais
+					conta.total.toFixed(3)
+					// get it go get table line
+					modify = item.identify
+					// update table
+					var newprice = price * item.qty
+					var grande = '<tr class="' + modify +'"><td>' + name + '</td>><td>' + item.qty + '</td><td><i id="' + modify + '" class="remove red icon" onClick="remove(this.id)"></i></td><td>' + newprice + '€</td></tr>'
+					modify = $("." + modify).replaceWith(grande)
+				}
 			})
+			// verificar se já existia um item ou não
+			if (control == 0) {
+				already.items.push({
+					name: name,
+					price: price,
+					qty: quantity,
+					identify: usar
+				})
+				// parse do valor
+				conta.total += parseFloat(price)
+				// valor só com duas casas decimais
+				conta.total.toFixed(3)
+				// linha a adicionar à tabela
+				var newprice = price.slice(0,-1) * quantity
+				var grande = '<tr class="' + usar +'"><td>' + name + '</td>><td>' + 1 + '</td><td><i id="' + usar + '" class="remove red icon" onClick="remove(this.id)"></i></td><td>' + newprice + '€</td></tr>'
+				// update table	& pedido
+				$('.ui.huge.table').append(grande)
+			}
 
 			// guarda item no sessionStorage
 			sessionStorage.setItem('refeicao', JSON.stringify(already))
 			sessionStorage.setItem('total', JSON.stringify(conta))
 
-			// linha a adicionar à tabela
-			var grande = '<tr class="' + usar +'"><td>' + name + '</td>><td>' + price + '</td><td><i id="' + usar + '" class="remove red icon" onClick="remove(this.id)"></i></td></tr>'
-
-			// update table	& pedido
-			$('.ui.huge.table').append(grande)
 			$('#total').text(conta.total + '€')
 			$('#pedido').html('<i class="shop icon"></i>Meu Pedido: ' + conta.total + '€')
 	  }
@@ -133,7 +161,7 @@ function remove(unique) {
 	jQuery.each(already.items, function (index, item) {
 		if (item.identify == unique) {
 			// update total
-			conta.total -= parseFloat(item.price.slice(0,-1))
+			conta.total -= parseFloat(item.price * item.qty)
 			// valor só com duas casas decimais
 			conta.total.toFixed(3)
 			// remove element from array
